8278e004079627066c368f74c3c55a169e98bea9
sam-huckaby <samhuckaby@gmail.com>
2023-07-03 13:23:33 -0400

    Fully translate site (sans content) into en, hi, sw, ja, pt-BR, and ru

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    description: z.string(),
     author: z.string(),
     date: z.date(),
   }),
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,18 +1,116 @@
 export const languages = {
   en: 'English',
-  // fr: 'Français',
+  hi: 'हिंदी',
+  ['pt-BR']: 'Português Brasileiro',
+  sw: 'Kiswahili',
+  ja: '日本語',
+  ru: 'Русский',
 };
 
 export const defaultLang = 'en';
 
+// Translations for UI components
 export const ui = {
   en: {
     'header.pageTitle': 'Crash Course Programming',
     'header.by': 'By',
     'header.author': 'Sam Huckaby',
+    'blog.linkTitle': 'Head to the Blog',
+    'blog.linkBody': 'Read more about programming on my blog.',
+    'blog.pageTitle': 'Crash Course Blog',
+    'blog.header': 'Most Recent Posts',
+    'post.watchYouTube': 'Watch the video on YouTube',
+    'home.header': 'I want to teach you how to write code',
+    'home.paragraph1':
+      "In today's day and age it has never been easier to enter the world of computer programming. Pretty much every adult has a computer that is capable of writing some kind of computer code, even if it's an old laptop that you've had forever.",
+    'home.paragraph2':
+      "All you need is a desire to learn and a willingness to keep trying when you don't get it right away. There is no magic bullet. You will get frustrated - that's true no matter what you're learning. If you're willing to take on this challenge, then I want to come alongside you and help.",
+    'home.paragraph3':
+      'This site is designed to be a place where you can come to look for helpful information for your journey to becoming a computer programmer. I will be posting semi-regularly as I build more course material, and will be posting across various topics from computer science.',
   },
-  // fr: {
-  //   'header.home': 'Accueil',
-  //   'header.about': 'À propos',
-  // },
+  hi: {
+    'header.pageTitle': 'क्रैश कोर्स प्रोग्रामिंग',
+    'header.by': 'द्वारा',
+    'header.author': 'सैम हकबी',
+    'blog.linkTitle': 'ब्लॉग की ओर जाएं',
+    'blog.linkBody': 'मेरे ब्लॉग पर प्रोग्रामिंग के बारे में और पढ़ें।',
+    'blog.pageTitle': 'क्रैश कोर्स ब्लॉग',
+    'blog.header': 'सबसे हाल के पोस्ट',
+    'post.watchYouTube': 'वीडियो को YouTube पर देखें',
+    'home.header': 'मैं आपको कोड लिखना सिखाना चाहता हूं',
+    'home.paragraph1':
+      'आज के दिन और युग में कंप्यूटर प्रोग्रामिंग की दुनिया में प्रवेश करना कभी भी आसान नहीं था। लगभग हर वयस्क के पास एक कंप्यूटर है जो किसी भी प्रकार का कंप्यूटर कोड लिखने में सक्षम है, भले ही यह एक पुराना लैपटॉप हो जिसे आपने हमेशा रखा है।',
+    'home.paragraph2':
+      'आपको सिर्फ एक इच्छा चाहिए होती है सीखने की और एक स्वीकार्यता जब आप तुरंत सही नहीं मिलते हैं। कोई जादुई गोली नहीं है। आप फ्रस्ट्रेट हो जाएंगे - यह सच है, चाहे आप कुछ भी सीख रहे हों। यदि आप इस चुनौती को स्वीकार करने के लिए तैयार हैं, तो मैं आपके साथ आना चाहता हूं और मदद करना चाहता हूं।',
+    'home.paragraph3':
+      'यह साइट ऐसी जगह के रूप में डिजाइन की गई है जहां आप कंप्यूटर प्रोग्रामर बनने की अपनी यात्रा के लिए उपयोगी जानकारी खोजने के लिए आ सकते हैं। मैं और अधिक कोर्स सामग्री बनाने के रूप में सेमी-नियमित रूप से पोस्ट करूंगा, और कंप्यूटर विज्ञान के विभिन्न विषयों पर पोस्ट करूंगा।',
+  },
+  ['pt-BR']: {
+    'header.pageTitle': 'Programação de Curso Intensivo',
+    'header.by': 'Por',
+    'header.author': 'Sam Huckaby',
+    'blog.linkTitle': 'Vá para o Blog',
+    'blog.linkBody': 'Leia mais sobre programação no meu blog.',
+    'blog.pageTitle': 'Blog do Curso Intensivo',
+    'blog.header': 'Posts Mais Recentes',
+    'post.watchYouTube': 'Assista o vídeo no YouTube',
+    'home.header': 'Quero ensinar você a programar',
+    'home.paragraph1':
+      'Nunca foi tão fácil entrar no mundo da programação de computadores. Praticamente todo adulto tem um computador capaz de escrever algum tipo de código, mesmo que seja um laptop antigo que você tem há muito tempo.',
+    'home.paragraph2':
+      'Tudo que você precisa é do desejo de aprender e da disposição para continuar tentando quando não acertar de primeira. Não existe solução mágica. Você vai se frustrar - isso é verdade, não importa o que você esteja aprendendo. Se você está disposto a encarar esse desafio, então eu quero estar ao seu lado e ajudar.',
+    'home.paragraph3':
+      'Este site foi projetado para ser um local onde você pode encontrar informações úteis para a sua jornada de se tornar um programador de computadores. Vou postar semi-regularmente conforme construo mais material do curso, e estarei postando sobre vários tópicos de ciência da computação.',
+  },
+  sw: {
+    'header.pageTitle': 'Programu ya Kozi Fupi',
+    'header.by': 'Na',
+    'header.author': 'Sam Huckaby',
+    'blog.linkTitle': 'Nenda kwenye Blogu',
+    'blog.linkBody': 'Soma zaidi kuhusu programu kwenye blogu yangu.',
+    'blog.pageTitle': 'Blogu ya Kozi Fupi',
+    'blog.header': 'Posts za Hivi Punde',
+    'post.watchYouTube': 'Tazama video kwenye YouTube',
+    'home.header': 'Nataka kukufundisha jinsi ya kuandika kificho',
+    'home.paragraph1':
+      'Katika dunia ya leo, haijawahi kuwa rahisi zaidi kuingia katika ulimwengu wa programu ya kompyuta. Karibu kila mtu mzima ana kompyuta inayoweza kuandika aina fulani ya msimbo wa kompyuta, hata ikiwa ni kompyuta ndogo ya zamani uliyokuwa nayo tangu zamani.',
+    'home.paragraph2':
+      'Unachohitaji ni tamaa ya kujifunza na uwezo wa kuendelea kujaribu unaposhindwa mara moja. Hakuna suluhisho la moja kwa moja. Utaghadhabika - ni kweli bila kujali unachojifunza. Ikiwa uko tayari kukabiliana na changamoto hii, basi nataka kuja pamoja nawe na kusaidia.',
+    'home.paragraph3':
+      'Tovuti hii imeundwa kuwa mahali ambapo unaweza kuja kutafuta habari za msaada kwa safari yako ya kuwa programu ya kompyuta. Nitakuwa naposti mara kwa mara nikiendelea kujenga nyenzo zaidi za kozi, na nitakuwa naposti kwenye mada anuwai za sayansi ya kompyuta.',
+  },
+  ja: {
+    'header.pageTitle': 'クラッシュコースプログラミング',
+    'header.by': 'によって',
+    'header.author': 'サム・ハッカビー',
+    'blog.linkTitle': 'ブログへ',
+    'blog.linkBody': 'ブログでプログラミングについてさらに読む。',
+    'blog.pageTitle': 'クラッシュコースブログ',
+    'blog.header': '最新の投稿',
+    'post.watchYouTube': 'YouTubeでビデオを見る',
+    'home.header': 'あなたにコードの書き方を教えたい',
+    'home.paragraph1':
+      '今日の時代、コンピュータプログラミングの世界に足を踏み入れることがこれほど簡単だったことはありません。ほとんどの大人は、長年持っていた古いラップトップであろうと、何らかの形でコンピュータコードを書くことができるコンピュータを持っています。',
+    'home.paragraph2':
+      '必要なのは学びたいという欲求と、すぐにうまくいかない時でも頑張り続ける意志だけです。魔法の弾丸はありません。何を学んでいても、挫折することは確かです。この挑戦を引き受ける意欲があるなら、私はあなたを支えたいと思います。',
+    'home.paragraph3':
+      'このサイトは、コンピュータプログラマーになるための旅路に役立つ情報を探し求める場所として設計されています。私はさらなるコース資料を作り上げるにつれて定期的に投稿を行い、コンピュータ科学の様々な話題について投稿していきます。',
+  },
+  ru: {
+    'header.pageTitle': 'Интенсивный курс программирования',
+    'header.by': 'От',
+    'header.author': 'Сэм Хакаби',
+    'blog.linkTitle': 'Перейти в блог',
+    'blog.linkBody': 'Узнайте больше о программировании в моем блоге.',
+    'blog.pageTitle': 'Блог о курсе',
+    'blog.header': 'Последние посты',
+    'post.watchYouTube': 'Посмотрите видео на YouTube',
+    'home.header': 'Я хочу научить вас писать код',
+    'home.paragraph1':
+      'В наше время никогда не было так просто войти в мир компьютерного программирования. Практически у каждого взрослого есть компьютер, на котором можно написать какой-то вид компьютерного кода, даже если это старый ноутбук, который у вас был вечно.',
+    'home.paragraph2':
+      'Все, что вам нужно, - это желание учиться и готовность продолжать пытаться, когда у вас сразу не получается. Нет волшебного пуля. Вы будете фрустрированы - это верно, независимо от того, что вы учитесь. Если вы готовы принять это вызов, то я хочу идти вместе с вами и помочь.',
+    'home.paragraph3':
+      'Этот сайт предназначен для того, чтобы вы могли найти полезную информацию для своего пути к становлению компьютерным программистом. Я буду регулярно публиковать по мере создания больше материалов для курса и буду публиковать на различные темы из области компьютерных наук.',
+  },
 } as const;
